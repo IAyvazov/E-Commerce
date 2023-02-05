51ff891cf0ebe4b45cfc3239aa7a596745edbfdb
IAyvazov <iayvazovl@gmail.com>
2023-02-05 19:26:13 +0200

    Initial Creating

---
+++ Infrastructure/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+using Core.Entities;
+
+namespace Infrastructure.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
